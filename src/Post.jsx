99330933f2e6c4f8939127e897d3cfb6a97878a2
bappah <mahammadmusa004@gmail.com>
2024-04-30 16:30:25 -0700

    Finish post page

---
+++ src/Post.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import img from './imageicon.png'
+
+export default function Post() {
+    const [imageUrl, setImageUrl] = useState(null);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImageUrl(reader.result);
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const title = e.target.elements.title.value.trim();
+        const author = e.target.elements.author.value.trim();
+        const category = e.target.elements.category.value.trim();
+        const content = e.target.elements.content.value.trim();
+        const image = imageUrl;
+
+        if (!title || !author || !category || !content || !image) {
+            alert('Please fill in all fields and upload an image.');
+            return;
+        }
+    };
+
+    return (
+        <div>
+            <form className="custom-form" onSubmit={handleSubmit}>
+                <h2>Title</h2>
+                <input type="text" name="title" placeholder="Enter title" />
+                <h2>Author</h2>
+                <input type="text" name="author" placeholder="Enter author" />
+                <h2>Category</h2>
+                <input type="text" name="category" placeholder="Enter category" />
+
+                <h2>Content</h2>
+                <textarea name="content" placeholder="Enter content" />
+                <h2>Upload image</h2>
+                <div className="file-input">
+                    <label htmlFor="file-upload" className="file-label">
+                        {imageUrl ? (
+                            <img src={imageUrl} alt="Upload Image" />
+                        ) : (
+                            <img src={img} alt="Upload Image" />
+                        )}
+                    </label>
+                    <input id="file-upload" type="file" onChange={handleImageChange} />
+                </div>
+
+                <input type="submit" value="Add blog" />
+            </form>
+        </div>
+    );
+}
